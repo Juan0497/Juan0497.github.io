e2722278c34045cc32eb4759f2ad6b7cbc84af0f
unknown <juan97f@gmail.com>
2021-08-17 03:41:41 -0600

    actualizar pc

--- src/components/Navbar/components/NavbarCV.jsx
+++ src/components/Navbar/components/NavbarCV.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Document, Page } from 'react-pdf'
-import { pdfjs } from 'react-pdf'
+import { Document, Page, pdfjs } from 'react-pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = '../node_modules/pdfjs-dist/build/pdf.worker.js'
 import PdfFile from '../../../pdf/Juan_Fernández_CV.pdf'
 import arrow from '../../../styles/imgs/iconos/iconobackarrow.png'
